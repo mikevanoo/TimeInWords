7eb84c7fb30fdee67b69ab278382a34b030aebd2
Mike van Oorschot <mikevanoo@gmail.com>
2026-02-06 12:24:58 +0000

    Refactored duplicate tests for TimeGrids into a shared base class.

---
+++ tests/TextToTimeGridLib.Tests/Grids/BaseTimeGridShould.cs
@@ -0,0 +1,22 @@
+﻿using TextToTimeGridLib.Grids;
+
+namespace TextToTimeGridLib.Tests.Grids;
+
+public abstract class BaseTimeGridShould<T>
+    where T : TimeGrid, new()
+{
+    [Fact]
+    public void ProvideRawGridOfCorrectSize()
+    {
+        const int ExpectedNumberOfLines = 10;
+        const int ExpectedLineLength = 11;
+
+        var grid = new T();
+
+        var actual = grid.ToString();
+
+        var lines = actual.Split('\n');
+        lines.Should().HaveCount(ExpectedNumberOfLines);
+        lines.Should().AllSatisfy(line => line.Should().HaveLength(ExpectedLineLength));
+    }
+}
--- tests/TextToTimeGridLib.Tests/Grids/TimeGridDutchShould.cs
+++ tests/TextToTimeGridLib.Tests/Grids/TimeGridDutchShould.cs
@@ -2,17 +2,4 @@
 
 namespace TextToTimeGridLib.Tests.Grids;
 
-public class TimeGridDutchShould
-{
-    [Fact]
-    public void ProvideRawGridOfCorrectSize()
-    {
-        var grid = new TimeGridDutch();
-
-        var actual = grid.ToString();
-
-        var lines = actual.Split('\n');
-        lines.Should().HaveCount(10);
-        lines.Should().AllSatisfy(line => line.Should().HaveLength(11));
-    }
-}
+public class TimeGridDutchShould : BaseTimeGridShould<TimeGridDutch> { }
