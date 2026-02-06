b40c9d0c3da6abd78e40201953094c28bd3be5a3
Mike van Oorschot <mikevanoo@gmail.com>
2026-02-06 13:46:01 +0000

    Added FrenchPreset to TimeToTextLib with corresponding tests.

--- tests/TimeToTextLib.Tests/Presets/TimeToTextShould.cs
+++ tests/TimeToTextLib.Tests/Presets/TimeToTextShould.cs
@@ -21,4 +21,14 @@ public class TimeToTextShould
 
         actual.Should().BeEquivalentTo(expected);
     }
+
+    [Fact]
+    public void ReturnFrenchTimeToText()
+    {
+        var actual = TimeToText.GetSimple(LanguagePreset.Language.French, new DateTime(2024, 1, 1, 12, 7, 0));
+
+        var expected = new TimeToTextFormat { TimeAsText = "IL EST DOUZE HEURES CINQ", AdditionalMinutes = 2 };
+
+        actual.Should().BeEquivalentTo(expected);
+    }
 }
